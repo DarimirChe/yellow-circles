fb6e5fc7f14eea6d35fd793e1f929d0022416377
Darimir <darimrulit@gmail.com>
2024-11-30 12:10:19 +0300

    The interface is connected as a separate class. The program draws circles of random color.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
+from UI import Ui_Form
 from random import randint
-from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication
 
@@ -9,14 +9,15 @@ from PyQt6.QtWidgets import QWidget, QApplication
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi("UI.ui", self)
-        self.pushButton.clicked.connect(self.update)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.update)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         diameter = randint(10, 150)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
         qp.drawEllipse(50, 50, diameter, diameter)
         qp.end()
 
