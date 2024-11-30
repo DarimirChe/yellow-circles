540a316daaa0a369d4139910003f0d65f7345c0c
Darimir <darimrulit@gmail.com>
2024-11-30 12:00:00 +0300

    After pressing the button, circles of yellow color and random diameter appear on the form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 
+from random import randint
 from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication
 
 
@@ -8,6 +10,15 @@ class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
         uic.loadUi("UI.ui", self)
+        self.pushButton.clicked.connect(self.update)
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        diameter = randint(10, 150)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(50, 50, diameter, diameter)
+        qp.end()
 
 
 if __name__ == "__main__":
